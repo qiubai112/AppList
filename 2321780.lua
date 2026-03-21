af1462e9c9a3c1ecb9f5b512b58a14ab57d5b756
qiubai112 <1974462115@qq.com>
2026-03-21 11:17:57 +0800

    Add 2321780.lua

---
+++ 2321780.lua
@@ -0,0 +1,18 @@
+-- 2321780's Lua and Manifest Created by Morrenus
+-- Starship Troopers: Ultimate Bug War!
+-- Created: March 16, 2026 at 13:19:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2321780, 1, "26a51ade576dac78e9518e74a353d504acd467a0f430e582b90355ad1b784c0a") -- Starship Troopers: Ultimate Bug War!
+-- MAIN APP DEPOTS
+addappid(2321781, 1, "cb775e098e46a6f68f2d02dbb1c9095df4afaa034fa74666ac66de8e6e06fa4d") -- Depot 2321781
+setManifestid(2321781, "5939599801628694608", 7847887186)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
