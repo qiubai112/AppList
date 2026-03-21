d31cfb33accf43aef48262307528a301fff210bd
qiubai112 <1974462115@qq.com>
2026-03-21 11:18:21 +0800

    Add 3743790.lua

---
+++ 3743790.lua
@@ -0,0 +1,25 @@
+-- 3743790's Lua and Manifest Created by Morrenus
+-- Thomas & Friends™: Wonders of Sodor
+-- Created: March 18, 2026 at 08:01:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3743790) -- Thomas & Friends™: Wonders of Sodor
+-- MAIN APP DEPOTS
+addappid(3743791, 1, "319047ea8ff14b6423edf068bcf6e172e2b49ab644eb998e8fd2ad65507deaae") -- Depot 3743791
+setManifestid(3743791, "7583999811380419280", 8380950361)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Thomas  Friends Wonders of Sodor  James Add-On (AppID: 4262440)
+addappid(4262440)
+addappid(4262440, 1, "49e074e3f2a4311eba6c8daeefb9520cf9a68038b663af25e52e07fbb7a61548") -- Thomas  Friends Wonders of Sodor  James Add-On - Depot 4262440
+setManifestid(4262440, "4303264831270393653", 5884)
