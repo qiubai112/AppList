fc90df3df88e1ea1c9ff7184a63a50b5511abac8
qiubai112 <1974462115@qq.com>
2026-03-21 11:18:12 +0800

    Add 3097300.lua

---
+++ 3097300.lua
@@ -0,0 +1,14 @@
+-- 3097300's Lua and Manifest Created by Morrenus
+-- Creature Kitchen
+-- Created: March 18, 2026 at 14:12:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3097300, 1, "b80e6cf9b4525e59ec48d7e6fba1d6b092562c29bb0402b8aa1abce9d144834b") -- Creature Kitchen
+-- MAIN APP DEPOTS
+addappid(3097301, 1, "c8c54f6efe8df976c29b33df0721d303accecd42f0fecc22756501b59fae2be3") -- Depot 3097301
+setManifestid(3097301, "6310614589660123434", 945709511)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3097302) -- Depot 3097302 (empty depot)
