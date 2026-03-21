d960a150bd182221333a44987d4639b087e719b8
qiubai112 <1974462115@qq.com>
2026-03-21 11:18:04 +0800

    Add 2835650.lua

---
+++ 2835650.lua
@@ -0,0 +1,12 @@
+-- 2835650's Lua and Manifest Created by Morrenus
+-- Randomice
+-- Created: March 13, 2026 at 06:12:24 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2835650) -- Randomice
+-- MAIN APP DEPOTS
+addappid(2835651, 1, "a675efd8945eed1285b6da4098744156c3084d5ae1e1ecfe362000e041131ba1") -- Depot 2835651
+setManifestid(2835651, "8168192972172892544", 1087736982)
