ceb14e275ca242ceecf65346aaddf8d05203512b
qiubai112 <1974462115@qq.com>
2026-03-21 11:18:29 +0800

    Add 3924170.lua

---
+++ 3924170.lua
@@ -0,0 +1,12 @@
+-- 3924170's Lua and Manifest Created by Morrenus
+-- Eternal Afternoon
+-- Created: March 20, 2026 at 23:15:09 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3924170) -- Eternal Afternoon
+-- MAIN APP DEPOTS
+addappid(3924172, 1, "4ae45c6d92e9b581bb50228ab01aea2c3652d0e03ad974ba9fc92f1a0f63ce1e") -- Depot 3924172
+setManifestid(3924172, "6895284996528382677", 728906327)
