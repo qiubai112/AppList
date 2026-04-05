59ef73edb86831de13129423e97f90f5c053798c
qiubai112 <1974462115@qq.com>
2026-04-05 21:27:42 +0800

    Add 4286550.lua

---
+++ 4286550.lua
@@ -0,0 +1,12 @@
+-- 4286550's Lua and Manifest Created by Morrenus
+-- Keep on Mining! - Worlds
+-- Created: April 05, 2026 at 09:27:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4286550, 1, "2eb48304e26ac2d620c4ccbe213c65aa642b8689ae29eb47698830589d401eaf") -- Keep on Mining! - Worlds
+-- MAIN APP DEPOTS
+addappid(4286551, 1, "2500be67ef3fc3ff04d580bc360128433d6d9351b464524dad7dc2257c6d2918") -- Depot 4286551
+setManifestid(4286551, "3255739399303062356", 607120150)
