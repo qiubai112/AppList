ea5708e6fadae9ba3fafc2dc37cc5f02e402e14d
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:34 +0800

    Add 3181650.lua

---
+++ 3181650.lua
@@ -0,0 +1,22 @@
+-- 3181650's Lua and Manifest Created by Hubcap Manifest
+-- MINOS
+-- Created: April 14, 2026 at 14:22:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3181650) -- MINOS
+-- MAIN APP DEPOTS
+addappid(3181651, 1, "91d8d483b86332b4ba5c069699f0f9baa66d40aa6176029e2e862c789d4743cd") -- Depot 3181651
+setManifestid(3181651, "167309562110994189", 3924644330)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Minos Supporter Pack (AppID: 4359010) - missing depot keys
+-- addappid(4359010)
