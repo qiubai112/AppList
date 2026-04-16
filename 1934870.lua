0c15a82504a0a46d22da9975cd8674f3c66af598
qiubai112 <1974462115@qq.com>
2026-04-16 09:59:57 +0800

    Add 1934870.lua

---
+++ 1934870.lua
@@ -0,0 +1,12 @@
+-- 1934870's Lua and Manifest Created by Hubcap Manifest
+-- Wirm
+-- Created: February 17, 2026 at 06:24:31 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1934870) -- Wirm
+-- MAIN APP DEPOTS
+addappid(1934871, 1, "99a9ba0f99b7d3e571442fde6e8f77bddb5827e208326d6fa1ea448ded3d327e") -- Depot 1934871
+setManifestid(1934871, "4751962186894796839", 27599583)
