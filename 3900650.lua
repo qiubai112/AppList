d1c8d2903fb93dbd4180c4c60a01a088dc6144b6
qiubai112 <1974462115@qq.com>
2026-04-16 10:01:07 +0800

    Add 3900650.lua

---
+++ 3900650.lua
@@ -0,0 +1,18 @@
+-- 3900650's Lua and Manifest Created by Hubcap Manifest
+-- Spider Roulette
+-- Created: April 15, 2026 at 20:30:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3900650) -- Spider Roulette
+-- MAIN APP DEPOTS
+addappid(3900651, 1, "dfcff8d2fc50aff51096d46a49e91eb18f6c22ee84d1e00b93f0c6645f6f88a5") -- Depot 3900651
+setManifestid(3900651, "67851228552891282", 1390043980)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
