076325ad4fab2142e5d428e4bfb31c7d10f92df3
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:16 +0800

    Update 2947440.lua

--- 2947440.lua
+++ 2947440.lua
@@ -1,3 +1,34 @@
-addappid(2947440)
-addappid(2947441, 1, "5329bd39507637997739c33a4b5737f9539ee434a7b79bd875b5fc7d1337c684")
-setManifestid(2947441,"2333475478848025339")
+-- 2947440's Lua and Manifest Created by Hubcap Manifest
+-- SILENT HILL f
+-- Created: December 09, 2025 at 00:56:06 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2947440) -- SILENT HILL f
+-- MAIN APP DEPOTS
+addappid(2947441, 1, "5329bd39507637997739c33a4b5737f9539ee434a7b79bd875b5fc7d1337c684") -- Depot 2947441
+setManifestid(2947441, "2588355430949594890", 47039677886)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- SILENT HILL f - Bonus Content (AppID: 3282720)
+addappid(3282720)
+addtoken(3282720, "186020997252537705")
+addappid(3282721, 1, "fd38a70c015f046a880fc4db0487fcccdf05e009b286cc4175abdddf61511342") -- SILENT HILL f - Bonus Content - Depot 3282721
+setManifestid(3282721, "9044674092896486181", 3328222109)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3282160) -- SILENT HILL f - White Sailor School Uniform
+addtoken(3282160, "1723734955608826062")
+addappid(3282170) -- SILENT HILL f - Item Pack
+addtoken(3282170, "8745338579199605697")
+addappid(3282180) -- SILENT HILL f - Pink Rabbit Costume
+addtoken(3282180, "2435190140460799412")
+addappid(3282190) -- SILENT HILL f - Omamori Peony
+addtoken(3282190, "17422305961579312315")
+addappid(3516200) -- SILENT HILL f - Digital Deluxe Upgrade
