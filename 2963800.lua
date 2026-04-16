ba58d807a36178361ee288a50723d11aafad94a7
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:25 +0800

    Add 2963800.lua

---
+++ 2963800.lua
@@ -0,0 +1,18 @@
+-- 2963800's Lua and Manifest Created by Hubcap Manifest
+-- DrainSim
+-- Created: April 13, 2026 at 17:12:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2963800) -- DrainSim
+-- MAIN APP DEPOTS
+addappid(2963801, 1, "378db61e0fba6945aa38162f21ab98cd80c6f68a85a64b0bdeffc76895b8fdbb") -- Depot 2963801
+setManifestid(2963801, "8658005053649680482", 3725300334)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
