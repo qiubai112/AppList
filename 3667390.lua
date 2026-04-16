7b6bc9f7c29634c33c47598a5f50302a60905469
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:59 +0800

    Add 3667390.lua

---
+++ 3667390.lua
@@ -0,0 +1,12 @@
+-- 3667390's Lua and Manifest Created by Hubcap Manifest
+-- Black Hole Fishing
+-- Created: April 15, 2026 at 20:31:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3667390, 1, "7463da0ff05226306c827f8cd6f6e74bdbfc99e6efd398716279753b5bdfcbf7") -- Black Hole Fishing
+-- MAIN APP DEPOTS
+addappid(3667391, 1, "de2bcd10a6f1e71570a2dc91eb39fd8087b4f17b4affe4a53b8e6bfd53b9d661") -- Depot 3667391
+setManifestid(3667391, "991778199623125615", 254679932)
