f27aee78ed6f3df221fc4d815c2e29a8ebd0e828
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:06 +0800

    Update 2738490.lua

--- 2738490.lua
+++ 2738490.lua
@@ -1,4 +1,14 @@
-addappid(2738490)
-addappid(2738491, 1, "27cff6c7bfcd289e04d87efd458564e1cb038e14ccd8fa71717536a221c7f0d5")
-setManifestid(2738491,"8321989289072606466")
-setManifestid(2738492,"8339463034453626540")
+-- 2738490's Lua and Manifest Created by Hubcap Manifest
+-- Sol Cesto
+-- Created: April 13, 2026 at 11:12:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2738490, 1, "7e60d74c4b37087ffb0091a83fdffb6981c20a88a73ec112ff15ca65eead95ab") -- Sol Cesto
+-- MAIN APP DEPOTS
+addappid(2738491, 1, "27cff6c7bfcd289e04d87efd458564e1cb038e14ccd8fa71717536a221c7f0d5") -- Depot 2738491
+setManifestid(2738491, "363498025658020710", 588996180)
+addappid(2738492, 1, "8f6cad90330e6be749671d5f2119fd80cfaf899e45dc1358b705ae4037db8036") -- Depot 2738492
+setManifestid(2738492, "8877324418763649144", 561841504)
