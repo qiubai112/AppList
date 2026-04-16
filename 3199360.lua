dae9974a164eea83e6a4ac42ecc4e9c0ffa2c07a
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:42 +0800

    Add 3199360.lua

---
+++ 3199360.lua
@@ -0,0 +1,18 @@
+-- 3199360's Lua and Manifest Created by Hubcap Manifest
+-- Skull Horde
+-- Created: April 15, 2026 at 20:05:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3199360) -- Skull Horde
+-- MAIN APP DEPOTS
+addappid(3199361, 1, "c2b0ecc12b01586a040ff61309536f7975baa1adaa1607217895ea80e41c4a0a") -- Depot 3199361
+setManifestid(3199361, "7160975666858542030", 996864976)
+addappid(3199362, 1, "6963e77017db85aac428308b6637241b6ea319afadca2009f0508e107803d735") -- Depot 3199362
+setManifestid(3199362, "4456323739579837385", 983639166)
+addappid(3199363, 1, "84b35cc8f40ef40aad898ab9b50a34e5df0104ebe3f97a3dc62e0308c3fbe057") -- Depot 3199363
+setManifestid(3199363, "8225435789466780467", 991122734)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4534160) -- Skull Horde - Supporters Pack
