7b01c19a5af04cd261216e526033346f29a984e0
qiubai112 <1974462115@qq.com>
2026-04-16 10:00:51 +0800

    Add 3548040.lua

---
+++ 3548040.lua
@@ -0,0 +1,12 @@
+-- 3548040's Lua and Manifest Created by Hubcap Manifest
+-- Machine Child
+-- Created: April 12, 2026 at 02:56:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3548040, 1, "bd424f21f203dcd6a6c01f1e6455d673ad2286d7ae9414d312030a6d25ab6319") -- Machine Child
+-- MAIN APP DEPOTS
+addappid(3548041, 1, "7e641c43dfc0803f6ae4a451a1debd5643f16117aed2aebf2ebb99da70e4ee07") -- Depot 3548041
+setManifestid(3548041, "4097664968010474545", 2811931829)
